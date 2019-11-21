b16ffc751b1c91cf7799989d48adca3061e39c32
github username <randyherasme6@gmail.com>
2019-11-21 14:20:39 -0500

    new user + fixed contrllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,23 @@
 class UsersController < ApplicationController
-    skip_before_action :authorized, only: [:create]
-
-    def profile
-        render json: { user: current_user }, status: :accepted
-    end
+  skip_before_action :authorized, only: [:create]
  
-    def create
-      @user = User.create(user_params)
-      if @user.valid?
-        @token = encode_token(user_id: @user.id)
-        render json: { user: @user, jwt: @token }, status: :created
-      else
-        render json: { error: 'failed to create user' }, status: :not_acceptable
-      end
+  def profile
+    render json: { user: current_user }, status: :accepted
+  end
+ 
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: @user, jwt: @token }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
-   
-    private
-   
-    def user_params
-      params.require(:user).permit(:username, :password, :bio, :avatar)
-    end
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:username, :password, :bio, :avatar)
+  end
 end
